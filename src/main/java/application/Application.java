53e73af11ebed81acbdfd30b74968703983528cf
tiger20111 <nikit-proc@mail.ru>
2021-05-17 02:22:11 +0300

    found bug

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -11,18 +11,23 @@ public class Application {
     public static void main(String[] args) {
         ExecutorService executorService;
         final int[] arrayE = getExampleArray1();
-        //printArray(arrayE);
+        printArray(arrayE);
         final long[] time = new long[4];
-        time[0] = runSingle(arrayE);
-        //printArray(arrayE);
+        int[] array;
+        array = arrayE.clone();
+        time[0] = runSingle(array);
+        printArray(array);
+        System.out.println("----------");
         for (int i = 2; i < 5; i++) {
             executorService = Executors.newFixedThreadPool(i);
             MultiQuickSort quickSort = new MultiQuickSort(executorService);
+            array = arrayE.clone();
+            printArray(array);
             long startTime = System.nanoTime();
-            quickSort.sort(arrayE, 10);
-            //printArray(arrayE);
+            quickSort.sort(array, 10);
             executorService.shutdown();
             long endTime = System.nanoTime();
+            printArray(array);
             time[i - 1] = endTime - startTime;
             System.out.println("Threads: " + i + ", time with " + (endTime - startTime));
         }
@@ -31,14 +36,14 @@ public class Application {
     private static int[] getExampleArray1() {
         Random rn = new Random();
         int n = 1000;
-        int[] array = new int[10000000];
+        int[] array = new int[40];
         for (int i = 0; i < array.length - 1; i++) {
             array[i] = rn.nextInt(n);
         }
         return array;
     }
 
-    private static long runSingle(final int[] array) {
+    private static long runSingle(int[] array) {
         long startTime = System.nanoTime();
         SingleQuickSort.sort(array);
         long endTime = System.nanoTime();
