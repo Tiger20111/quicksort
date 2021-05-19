74b881ebe279b49b601e0797617e73ab16270885
tiger20111 <nikit-proc@mail.ru>
2021-05-19 11:15:10 +0300

    finished correct

--- src/main/java/algorithm/multi/MultiQuickSort.java
+++ src/main/java/algorithm/multi/MultiQuickSort.java
@@ -1,6 +1,7 @@
 package algorithm.multi;
 
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MultiQuickSort {
     private final ExecutorService executorService;
@@ -8,15 +9,21 @@ public class MultiQuickSort {
         this.executorService = executorService;
     }
 
-    public void sort(final int[] array, int delta) {
-        sort(array, 0, array.length - 1, delta);
+    public void sort(int[] array, int delta, AtomicInteger tasks) {
+        sort(array, 0, array.length - 1, delta, tasks, 0);
     }
 
-    private void sort(final int[] array, int l, int r, final int delta) {
+    private void sort(int[] array, int l, int r, final int delta, AtomicInteger tasks, int smallT) {
         if (array.length == 0) {
+            if (smallT == 0) {
+                tasks.addAndGet(-1);
+            }
             return;
         }
         if (l >= r) {
+            if (smallT == 0) {
+                tasks.addAndGet(-1);
+            }
             return;
         }
 
@@ -38,26 +45,42 @@ public class MultiQuickSort {
                 i++;
             }
         }
-        if (l < j && (j - l < delta)) {
-            sort(array, l, j, delta);
-        } else {
-            final int finalJ = j;
-            executorService.submit(
-                    () -> sort(array, l, finalJ, delta)
-            );
+        if (l < j) {
+            if (j - l < delta) {
+                sort(array, l, j, delta, tasks, smallT + 1);
+            } else {
+                tasks.addAndGet(1);
+                final int finalJ = j;
+                executorService.submit(
+                        () -> sort(array, l, finalJ, delta, tasks, smallT)
+                );
+            }
         }
-        if (r > i && (r - i < delta)) {
-            sort(array, i, r, delta);
-        } else {
-            int finalI = i;
-            executorService.submit(
-                    () -> sort(array, finalI, r, delta)
-            );
+        if (r > i) {
+            if (r - i < delta) {
+                sort(array, i, r, delta, tasks, smallT + 1);
+            } else {
+                tasks.addAndGet(1);
+                int finalI = i;
+                executorService.submit(
+                        () -> sort(array, finalI, r, delta, tasks, smallT)
+                );
+            }
+        }
+        if (smallT == 0) {
+            tasks.addAndGet(-1);
         }
     }
 
     private static int getSupportElement(int[] array, int l, int r) {
         int supportIndex = l + (r - l) / 2;
         return array[supportIndex];
+    }
+
+    private static void printArray(int[] array) {
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
     }
 }
--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -3,31 +3,38 @@ package application;
 import algorithm.multi.MultiQuickSort;
 import algorithm.single.SingleQuickSort;
 
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ExecutorService executorService;
         final int[] arrayE = getExampleArray1();
-        printArray(arrayE);
+        //printArray(arrayE);
         final long[] time = new long[4];
         int[] array;
         array = arrayE.clone();
         time[0] = runSingle(array);
-        printArray(array);
+        //printArray(array);
         System.out.println("----------");
         for (int i = 2; i < 5; i++) {
             executorService = Executors.newFixedThreadPool(i);
+            AtomicInteger tasks = new AtomicInteger(1);
             MultiQuickSort quickSort = new MultiQuickSort(executorService);
             array = arrayE.clone();
-            printArray(array);
+            //printArray(array);
             long startTime = System.nanoTime();
-            quickSort.sort(array, 10);
+            quickSort.sort(array, 40, tasks);
+            while (tasks.get() > 0);
             executorService.shutdown();
+           // executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
             long endTime = System.nanoTime();
-            printArray(array);
+            //printArray(array);
             time[i - 1] = endTime - startTime;
             System.out.println("Threads: " + i + ", time with " + (endTime - startTime));
         }
@@ -36,10 +43,11 @@ public class Application {
     private static int[] getExampleArray1() {
         Random rn = new Random();
         int n = 1000;
-        int[] array = new int[40];
+        int[] array = new int[10000000];
         for (int i = 0; i < array.length - 1; i++) {
             array[i] = rn.nextInt(n);
         }
+        array[2] = 0;
         return array;
     }
 
